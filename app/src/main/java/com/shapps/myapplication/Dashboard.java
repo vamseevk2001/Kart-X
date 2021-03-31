706cf0fa556c79242eacc79bce49decf9381ac35
SHARVESH.S <sharveshsreethar0806@gmail.com>
2021-03-31 20:02:56 +0530

    added image6

--- app/src/main/java/com/shapps/myapplication/Dashboard.java
+++ app/src/main/java/com/shapps/myapplication/Dashboard.java
@@ -19,7 +19,7 @@ public class Dashboard extends AppCompatActivity {
     String img_url3 = "https://images-na.ssl-images-amazon.com/images/I/81v7oPP75kL._SL1500_.jpg"; //sony
     String img_url4 = "https://images-na.ssl-images-amazon.com/images/I/71b0AbTo9RL._SL1500_.jpg"; //boat
     String img_url5 = "https://store.storeimages.cdn-apple.com/4668/as-images.apple.com/is/airpods-max-select-skyblue-202011?wid=470&hei=556&fmt=png-alpha&.v=1604022365000";
-
+    String img_url6 = "https://images-na.ssl-images-amazon.com/images/I/71b0AbTo9RL._SL1500_.jpg";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
