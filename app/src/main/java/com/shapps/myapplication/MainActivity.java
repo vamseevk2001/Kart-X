cff643f91e4c14f7ea34ee85d5bdd911f2fdc4aa
vamseevk <vamseevk2001@gmail.cmo>
2021-04-08 21:02:53 +0530

    sd;ls

--- app/src/main/java/com/shapps/myapplication/MainActivity.java
+++ app/src/main/java/com/shapps/myapplication/MainActivity.java
@@ -2,12 +2,10 @@ package com.shapps.myapplication;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
-
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Toast;
-
 import com.google.android.gms.auth.api.signin.GoogleSignIn;
 import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
 import com.google.android.gms.auth.api.signin.GoogleSignInClient;
--- app/src/main/java/com/shapps/myapplication/cart.java
+++ app/src/main/java/com/shapps/myapplication/cart.java
@@ -1,5 +1,6 @@
 package com.shapps.myapplication;
 
+import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
@@ -10,5 +11,7 @@ public class cart extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_cart);
+        ActionBar actionBar = getSupportActionBar();
+        actionBar.hide();
     }
 }
