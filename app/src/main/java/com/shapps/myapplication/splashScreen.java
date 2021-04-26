49f9f7c79659e4afe3dab41da54ed0b38df6792d
vamseevk <vamseevk2001@gmail.cmo>
2021-04-26 21:50:04 +0530

    added splash screen

---
+++ app/src/main/java/com/shapps/myapplication/splashScreen.java
@@ -0,0 +1,42 @@
+package com.shapps.myapplication;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.media.MediaPlayer;
+import android.net.Uri;
+import android.os.Bundle;
+import android.widget.VideoView;
+
+import java.net.URI;
+
+public class splashScreen extends AppCompatActivity {
+
+    VideoView videoSplashScreen;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash_screen);
+
+        videoSplashScreen = findViewById(R.id.videoSplashScreen);
+        Uri video = Uri.parse("android.resource://" + getPackageName() + "/" + R.raw.kartxportrait);
+        videoSplashScreen.setVideoURI(video);
+        videoSplashScreen.setOnCompletionListener(new MediaPlayer.OnCompletionListener() {
+            @Override
+            public void onCompletion(MediaPlayer mp) {
+                startNextActivity();
+            }
+        });
+
+        videoSplashScreen.start();
+    }
+
+    private void startNextActivity() {
+        if (isFinishing())
+            return;
+        Intent intent = new Intent(this, MainActivity.class);
+        startActivity(intent);
+        finish();
+    }
+}
