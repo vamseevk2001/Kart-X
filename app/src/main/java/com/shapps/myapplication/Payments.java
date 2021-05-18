461530d85cb4aedca806b9757df4800debc094b7
Sharvesh S <sharveshsreethar0806@gmail.com>
2021-05-18 13:14:05 +0530

    Cleaned payments

--- app/src/main/java/com/shapps/myapplication/Payments.java
+++ app/src/main/java/com/shapps/myapplication/Payments.java
@@ -5,40 +5,29 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.app.Activity;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.Button;
-import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
-
 import com.razorpay.Checkout;
 import com.razorpay.PaymentResultListener;
-
 import org.json.JSONException;
 import org.json.JSONObject;
 
 public class Payments extends AppCompatActivity implements PaymentResultListener {
 private TextView amount;
-private Button pay;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_payments);
         amount=findViewById(R.id.amt);
-        pay=findViewById(R.id.pay);
         long amt = getIntent().getLongExtra("total", 0);
         amount.setText(String.valueOf(amt));
     }
 
     public void Gpay(View view) {
         String amt = (String) amount.getText();
-        if(amt.isEmpty()){
-            Toast.makeText(this,"Enter Amount",Toast.LENGTH_SHORT).show();
-        }
-
-        else{
             startPayment(amt);
-        }
+
     }
 
     private void startPayment(String amount) {
@@ -49,7 +38,6 @@ private Button pay;
             object.put("name","Sharvesh");
             object.put("description","Payment From Cart");
             double amo=Double.parseDouble(amount);
-            amo=amo*100;
             object.put("amount",amo);
             object.put("currency","INR");
             object.put("prefill.contact","7904592032");
